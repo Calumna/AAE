a22daa20113a1ba4f280493c7b123c30070e2174
Margot Noyelle <margot.noyelle63@gmail.com>
2023-06-19 18:48:41 -0400

    chat box (to do: add request)

--- messagerie-front/src/components/chat/topic.tsx
+++ messagerie-front/src/components/chat/topic.tsx
@@ -3,19 +3,39 @@ import {useParams} from "react-router-dom";
 import {MessageData} from "../../types";
 import {getMessages} from "./topics";
 import Message from "./message";
-import { List } from "@mui/material";
-
-
+import {Divider, Fab, FormControl, Grid, List, TextField} from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 const Topic = () => {
     let { topicId } = useParams();
     const [messages, setMessages] = useState<MessageData[]>([]);
 
+    const [messageToSend, setMessageToSend] = useState<string>('');
+    const onChangeMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMessageToSend(event.target.value);
+    }
+
+    const handleSend = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        if (messageToSend !== '') {
+            const newMessage: MessageData = {
+                userName: 'toto',
+                date: Date().toLocaleString(),
+                text: messageToSend
+            }
+            setMessages([
+                ...messages,
+                newMessage
+            ]);
+            setMessageToSend('');
+        }
+    }
+
     useEffect(() => {
         if (topicId !== undefined) {
             setMessages(getMessages(topicId));
         }
-    }, [messages, topicId]);
+    }, [topicId]);
 
     return (
         <div>
@@ -26,6 +46,31 @@ const Topic = () => {
                     ))}
                 </List>
             )}
+            <Grid
+                container
+                direction="row"
+                spacing={3}
+                alignItems="center"
+                 sx={{
+                     padding: '20px',
+                     bottom: '0',
+                     position: 'fixed'
+            }}>
+                <Grid item xs={11} component='form'>
+                    <TextField
+                        type="text"
+                        id="outlined-basic-email"
+                        placeholder="Type your message here"
+                        value={messageToSend}
+                        onChange={onChangeMessage}
+                        fullWidth
+                        multiline
+                        maxRows={4}/>
+                </Grid>
+                <Grid item xs={1}>
+                    <Fab onClick={handleSend} color="primary" aria-label="add"><SendIcon /></Fab>
+                </Grid>
+            </Grid>
         </div>
     )
 }
